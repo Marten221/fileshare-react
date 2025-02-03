35441f75dfcf2b27abda6bca25c14dbf02f80b1a
Marten221 <marten.ojasaar@ut.ee>
2025-02-03 11:23:00 +0200

    made ViewFilePage use useQuery ratcher than useEffect and made handleDownload use downloadMutation.

--- src/pages/ViewFilePage.jsx
+++ src/pages/ViewFilePage.jsx
@@ -3,58 +3,55 @@ import {useParams} from "react-router-dom";
 import {downloadFile, fetchFileDescription} from "../services/fileService";
 import FileView from "../components/FileView/FileView";
 import FormContainer from "../components/FormContainer";
-import { GoGear } from "react-icons/go";
-import { TiDeleteOutline } from "react-icons/ti";
+import {GoGear} from "react-icons/go";
+import {TiDeleteOutline} from "react-icons/ti";
+import {useMutation, useQuery} from "@tanstack/react-query";
+import ErrorMessage from "../components/ErrorMessage";
 
 const ViewFilePage = () => {
     const {fileId} = useParams();
-    const [file, setFile] = useState({})
-    const [loading, setLoading] = useState(true)
-    const [error, setError] = useState(null)
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const file = await fetchFileDescription(fileId);  // Add await here
-                setFile(file);
-            } catch (error) {
-                setError(error.message);
-            } finally {
-                setLoading(false)
-            }
-        };
 
-        fetchData()
-    }, [fileId]);
+    const {data: fileDescription, isError, error, isFetched} = useQuery({
+        queryKey: ["fileDescription"],
+        queryFn: () => fetchFileDescription(fileId)
+    })
+
 
     const handleDownload = async () => {
-        try {
-            setLoading(true);
-            const fileBlob = await downloadFile(fileId);
-            const blob = new Blob([fileBlob], {type: 'application/octet-stream'});
-            const url = window.URL.createObjectURL(blob);
-            const link = document.createElement('a');
-            link.href = url;
-            link.setAttribute('download', file.fileName + "." + file.fileExt)
-            document.body.appendChild(link);
-            link.click();
-            link.remove();
-            window.URL.revokeObjectURL(url);
-            setError("")
-        } catch (error) {
-            setError(error.message)
-        } finally {
-            setLoading(false);
-        }
+        const fileBlob = await downloadFile(fileId);
+        const blob = new Blob([fileBlob], {type: 'application/octet-stream'});
+        const url = window.URL.createObjectURL(blob);
+        const link = document.createElement('a');
+        link.href = url;
+        link.setAttribute('download', fileDescription.fileName + "." + fileDescription.fileExt)
+        document.body.appendChild(link);
+        link.click();
+        link.remove();
+        window.URL.revokeObjectURL(url);
     };
+
+    const downloadMutation = useMutation({
+        mutationFn: handleDownload,
+        onError: (error) => {
+            alert(error?.message);
+        },
+    })
+
+    if (isError) {
+        return (
+            <FormContainer>
+                <ErrorMessage message={error.message}/>
+            </FormContainer>
+        )
+    }
 
     return (
         <FormContainer>
             <FileView
-                handleDownload={handleDownload}
-                file={file}
-                error={error}
-                loading={loading}
+                downloadMutation={downloadMutation}
+                file={fileDescription}
+                isFetched={isFetched}
             />
         </FormContainer>
     );
